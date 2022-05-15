c5650e9e0881e02a82f47503c9f130b6c583c9ac
lpjones <105609518+lpjones@users.noreply.github.com>
2022-05-15 16:37:08 -0700

    Update pqueue.h

--- pqueue.h
+++ pqueue.h
@@ -57,34 +57,68 @@ class PQueue {
 
 // To be completed below
 // Return number of items in priority queue
+template <typename T, typename C = std::less<T>>
 size_t PQueue::Size() {
  return cur_size;
 }
 // Return top of priority queue
+template <typename T, typename C = std::less<T>>
 T& PQueue::Top() {
- return items[0];
+  if (!Size())
+    throw std::underflow_error("Empty priority queue!");
+  return items[Root()];
 }
 // Remove top of priority queue
+template <typename T, typename C = std::less<T>>
 void PQueue::Pop() {
- 
+  if (!Size())
+    throw std::underflow_error("Empty priority queue!");
+  items[Root()] = std::move(items[cur_size--]);
+  items.pop_back();
+  PercolateDown(Root());
 }
 // Insert item and sort priority queue
+template <typename T, typename C = std::less<T>>
 void PQueue::Push(const T &item) {
-
+    // Insert at the end
+    items.push_back(item);
+    cur_size++;
+    // Percolate up
+    PercolateUp(cur_size-1);
 }
 
-  // Helper methods for restructuring
-  void PercolateUp(size_t n) {
-  
+// Helper methods for restructuring
+template <typename T, typename C = std::less<T>>
+void PercolateUp(size_t n) {
+  while (HasParent(n) && CompareNodes(items[n], items[Parent(n)])) {
+    std::swap(items[Parent(n)], items[n]);
+    n = Parent(n);
   }
-  void PercolateDown(size_t n) {
-   
+}
+template <typename T, typename C = std::less<T>>
+void PercolateDown(size_t n) {
+  // While node has at least one child (if one, necessarily on the left)
+  while (IsNode(LeftChild(n))) {
+    // Consider left child by default
+    size_t child = LeftChild(n);
+    // If right child exists and smaller than left child, then consider right child
+    if (IsNode(RightChild(n)) && CompareNodes(items[RightChild(n)], items[LeftChild(n)]))
+      child = RightChild(n);
+    // Exchange smallest child with node to restore heap-order if necessary
+    if (CompareNodes(items[child], items[n]))
+      std::swap(items[child], items[n]);
+    else
+      break;
+    // Do it again, one level down
+    n = child;
   }
+}
 
-  // Node comparison
-  bool CompareNodes(size_t i, size_t j) {
-   
-  }
+// Node comparison
+template <typename T, typename C = std::less<T>>
+bool CompareNodes(size_t i, size_t j) {
+  return cmp(items[i], items[j]);
+}
 
 
 #endif  // PQUEUE_H_
