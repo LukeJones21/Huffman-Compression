eaf38866c24a988427f82391b1dfdb0e276fae84
inseon-0507 <82668821+inseon-0507@users.noreply.github.com>
2022-05-15 21:21:47 -0700

    Update test_pqueue.cc
    create one test case

--- test_pqueue.cc
+++ test_pqueue.cc
@@ -5,58 +5,72 @@
 #include "pqueue.h"
 
 TEST(PQueue, less) {
-  PQueue<int> pq;
+PQueue<int> pq;
 
-  pq.Push(42);
-  pq.Push(23);
-  pq.Push(2);
-  pq.Push(34);
+pq.Push(42);
+pq.Push(23);
+pq.Push(2);
+pq.Push(34);
 
-  EXPECT_EQ(pq.Top(), 2);
-  EXPECT_EQ(pq.Size(), 4);
-  pq.Pop();
-  EXPECT_EQ(pq.Top(), 23);
+EXPECT_EQ(pq.Top(), 2);
+EXPECT_EQ(pq.Size(), 4);
+pq.Pop();
+EXPECT_EQ(pq.Top(), 23);
 }
 
 TEST(PQueue, great) {
-  PQueue<int, std::greater<int>> pq;
+PQueue<int, std::greater<int>> pq;
 
-  pq.Push(42);
-  pq.Push(23);
-  pq.Push(2);
-  pq.Push(34);
+pq.Push(42);
+pq.Push(23);
+pq.Push(2);
+pq.Push(34);
 
-  EXPECT_EQ(pq.Top(), 42);
-  EXPECT_EQ(pq.Size(), 4);
-  pq.Pop();
-  EXPECT_EQ(pq.Top(), 34);
+EXPECT_EQ(pq.Top(), 42);
+EXPECT_EQ(pq.Size(), 4);
+pq.Pop();
+EXPECT_EQ(pq.Top(), 34);
 }
 
 class MyClass {
- public:
-  explicit MyClass(int n) : n_(n) {}
-  bool operator < (const MyClass &mc) const { return n_ < mc.n_; }
-  int n() { return n_; }
- private:
-  int n_;
+public:
+    explicit MyClass(int n) : n_(n) {}
+    bool operator < (const MyClass &mc) const { return n_ < mc.n_; }
+    int n() { return n_; }
+private:
+    int n_;
 };
 
 TEST(PQueue, custom_class) {
-  std::vector<MyClass> vec{MyClass(42), MyClass(23), MyClass(2), MyClass(34)};
+std::vector<MyClass> vec{MyClass(42), MyClass(23), MyClass(2), MyClass(34)};
 
-  PQueue<MyClass> pq;
-  pq.Push(vec[0]);
-  pq.Push(vec[1]);
-  pq.Push(vec[2]);
-  pq.Push(vec[3]);
+PQueue<MyClass> pq;
+pq.Push(vec[0]);
+pq.Push(vec[1]);
+pq.Push(vec[2]);
+pq.Push(vec[3]);
 
-  EXPECT_EQ(pq.Top().n(), vec[2].n());
-  EXPECT_EQ(pq.Size(), 4);
-  pq.Pop();
-  EXPECT_EQ(pq.Top().n(), vec[1].n());
+EXPECT_EQ(pq.Top().n(), vec[2].n());
+EXPECT_EQ(pq.Size(), 4);
+pq.Pop();
+EXPECT_EQ(pq.Top().n(), vec[1].n());
 }
 
-#if 0
+TEST(PQueue, ErrorThrow) {
+    PQueue<int> pq;
+
+    EXPECT_THROW(pq.Top(), std::exception);
+    EXPECT_THROW(pq.Pop(), std::exception);
+    pq.Push(1);
+    pq.Push(5);
+    pq.Push(10);
+    pq.Push(6);
+    EXPECT_EQ(pq.Top(), 1);
+    EXPECT_EQ(pq.Size(), 4);
+    pq.Pop();
+    EXPECT_EQ(pq.Top(), 5);
+}
+
 TEST(PQueue, custom_class_pointer) {
   std::vector<MyClass*> vec{new MyClass(42), new MyClass(23),
                             new MyClass(2), new MyClass(34)};
@@ -72,9 +86,8 @@ TEST(PQueue, custom_class_pointer) {
   pq.Pop();
   EXPECT_EQ(pq.Top(), vec[1]);
 }
-#endif
 
 int main(int argc, char *argv[]) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
